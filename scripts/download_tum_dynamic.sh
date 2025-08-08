68a0ccbb0e988360f7f5ee6f17d13e84d87bd713
yanyan-li <yanyan.li.camp@gmail.com>
2025-08-09 01:02:17 +0800

    first

--- scripts/download_tum_dynamic.sh
+++ scripts/download_tum_dynamic.sh
@@ -1,4 +1,13 @@
 mkdir -p datasets/tum
 cd datasets/tum
+
+
+wget https://cvg.cit.tum.de/rgbd/dataset/freiburg3/rgbd_dataset_freiburg3_sitting_xyz.tgz
+tar -xvzf rgbd_dataset_freiburg3_sitting_xyz.tgz
+
+
+wget https://cvg.cit.tum.de/rgbd/dataset/freiburg3/rgbd_dataset_freiburg3_sitting_static.tgz
+tar -xvzf rgbd_dataset_freiburg3_sitting_static.tgz
+
 wget https://cvg.cit.tum.de/rgbd/dataset/freiburg3/rgbd_dataset_freiburg3_walking_static.tgz
 tar -xvzf rgbd_dataset_freiburg3_walking_static.tgz
